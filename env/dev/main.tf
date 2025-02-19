0ed7d7493e0a0790bb100a1b3ccb977db127182a
Edube1008 <christina.njongwe@gmail.com>
2025-02-19 12:08:13 -0700

    added new branch

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source          = "./modules/s3"
+  source          = "./Modules/s3"
   bucket_name     = var.bucket_name
   lifecycle_days  = var.lifecycle_days
   bucket_prefix   = var.bucket_prefix
